905f5d105eeb53c018ded61f308596251a59caee
lukailun <957458041@qq.com>
2022-05-20 14:43:56 +0800

    Navigating to the Detail screen

--- app/src/main/java/com/yourcompany/android/landscapes/ui/CityListUi.kt
+++ app/src/main/java/com/yourcompany/android/landscapes/ui/CityListUi.kt
@@ -1,37 +1,3 @@
-/*
- * Copyright (c) 2022 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * This project and source code may use libraries or frameworks that are
- * released under various Open-Source licenses. Use of those libraries and
- * frameworks are governed by their own individual licenses.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.yourcompany.android.landscapes.ui
 
 import androidx.compose.foundation.Image
@@ -61,44 +27,44 @@ import com.yourcompany.android.landscapes.data.ViewModel
 @Preview(showSystemUi = true, device = Devices.PIXEL_4)
 @Composable
 fun CityListUi(
-  modifier: Modifier = Modifier,
-  viewModel: ViewModel = ViewModel(),
-  onCitySelected: (CityOverview) -> Unit = {}
+    modifier: Modifier = Modifier,
+    viewModel: ViewModel = ViewModel(),
+    onCitySelected: (CityOverview) -> Unit = {}
 ) {
 
-  Column(modifier = modifier.verticalScroll(rememberScrollState())) {
-    viewModel.cities.forEach { city ->
-      Card(
-        onClick = { onCitySelected(city) },
-        modifier = Modifier
-          .fillMaxWidth()
-          .padding(all = 8.dp),
-        elevation = 4.dp
-      ) {
-        Box(contentAlignment = Alignment.BottomEnd) {
-          Image(
-            painter = painterResource(id = city.image),
-            contentDescription = city.name,
-            contentScale = ContentScale.Crop
-          )
-          Column(
-            modifier = Modifier
-              .padding(all = 8.dp),
-            horizontalAlignment = Alignment.End
-          ) {
-            Text(
-              text = city.name,
-              color = Color.White,
-              style = MaterialTheme.typography.h5
-            )
-            Text(
-              text = "Picture by ${city.author}",
-              color = Color.White,
-              style = MaterialTheme.typography.caption
-            )
-          }
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        viewModel.cities.forEach { city ->
+            Card(
+                onClick = { onCitySelected(city) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 8.dp),
+                elevation = 4.dp
+            ) {
+                Box(contentAlignment = Alignment.BottomEnd) {
+                    Image(
+                        painter = painterResource(id = city.image),
+                        contentDescription = city.name,
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(all = 8.dp),
+                        horizontalAlignment = Alignment.End
+                    ) {
+                        Text(
+                            text = city.name,
+                            color = Color.White,
+                            style = MaterialTheme.typography.h5
+                        )
+                        Text(
+                            text = "Picture by ${city.author}",
+                            color = Color.White,
+                            style = MaterialTheme.typography.caption
+                        )
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
--- app/src/main/java/com/yourcompany/android/landscapes/ui/CompatUi.kt
+++ app/src/main/java/com/yourcompany/android/landscapes/ui/CompatUi.kt
@@ -1,37 +1,3 @@
-/*
- * Copyright (c) 2022 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * This project and source code may use libraries or frameworks that are
- * released under various Open-Source licenses. Use of those libraries and
- * frameworks are governed by their own individual licenses.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.yourcompany.android.landscapes.ui
 
 import androidx.compose.animation.AnimatedVisibility
@@ -56,16 +22,36 @@ import com.yourcompany.android.landscapes.data.ViewModel
 
 @Composable
 fun CompatUi(
-  navController: NavHostController,
-  viewModel: ViewModel = ViewModel(),
-  themeStore: ThemeStore
+    navController: NavHostController,
+    viewModel: ViewModel = ViewModel(),
+    themeStore: ThemeStore
 ) {
-  // TODO: Define bottomBarVisibility here
-  val items = listOf(Screen.List, Screen.Settings)
+    // TODO: Define bottomBarVisibility here
+    val items = listOf(Screen.List, Screen.Settings)
 
-  Scaffold(
-    // TODO: Define bottomBar here
-  ) { innerPadding ->
-    // TODO: Define NavHost here
-  }
+    Scaffold(
+        // TODO: Define bottomBar here
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.List.path,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = Screen.List.path) {
+                CityListUi(viewModel = viewModel,
+                    onCitySelected = {
+                        navController.navigate("detail/${it.name}")
+                    })
+            }
+            composable(route = "detail/{city}") { backstackEntry ->
+                val cityName =
+                    backstackEntry.arguments?.getString("city") ?: error("City is required")
+                val city = viewModel.cities.first { it.name == cityName }
+                CityDetailUi(viewModel = viewModel, city = city, isBigLayout = false)
+            }
+            composable(route = Screen.Settings.path) {
+                SettingsUi(themeStore = themeStore)
+            }
+        }
+    }
 }
